9d1a786c44a0d9eaec43f0233d61790ce2a2c59e
yin <yinzhili001@gmail.com>
2021-03-14 09:34:28 +0800

    范例

--- src/main/java/com/apifan/util/App.java
+++ src/main/java/com/apifan/util/App.java
@@ -1,11 +1,20 @@
 package com.apifan.util;
 
+import com.apifan.handler.FileHashHandler;
+import com.apifan.handler.FileNameHandler;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) throws Exception {
-        FileHashHandler handler = new FileHashHandler("E:\\tmp\\output");
-        handler.submit("E:\\media");
+        FileHashHandler hashHandler = new FileHashHandler();
+        hashHandler.setOutputPath("E:\\tmp\\new");
+        hashHandler.submit("E:\\media");
+
+        FileNameHandler nameHandler = new FileNameHandler();
+        nameHandler.setOutputPath("E:\\tmp\\new");
+        nameHandler.enableNoise();
+        nameHandler.submit("E:\\tmp");
     }
 }
