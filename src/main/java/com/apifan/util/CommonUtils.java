5ab6ec6cd527dacfead3d79ede3b8cfd63ade813
yinzl <yinzhili001@gmail.com>
2021-03-22 09:22:36 +0800

    读取二进制文件头

--- src/main/java/com/apifan/util/CommonUtils.java
+++ src/main/java/com/apifan/util/CommonUtils.java
@@ -2,14 +2,18 @@ package com.apifan.util;
 
 import com.github.promeg.pinyinhelper.Pinyin;
 import com.github.promeg.tinypinyin.lexicons.java.cncity.CnCityDict;
+import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.text.WordUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 通用工具
@@ -19,8 +23,64 @@ import java.nio.file.Files;
 public class CommonUtils {
     private static final Logger logger = LoggerFactory.getLogger(CommonUtils.class);
 
+    public static final Map<String, String> FILE_TYPE_MAP = new HashMap<>();
+
     static {
         Pinyin.init(Pinyin.newConfig().with(CnCityDict.getInstance()));
+
+        FILE_TYPE_MAP.put("ffd8ffe000104a464946", "jpg"); //JPEG (jpg)
+        FILE_TYPE_MAP.put("89504e470d0a1a0a0000", "png"); //PNG (png)
+        FILE_TYPE_MAP.put("47494638396126026f01", "gif"); //GIF (gif)
+        FILE_TYPE_MAP.put("49492a00227105008037", "tif"); //TIFF (tif)
+        FILE_TYPE_MAP.put("424d228c010000000000", "bmp"); //16色位图(bmp)
+        FILE_TYPE_MAP.put("424d8240090000000000", "bmp"); //24位位图(bmp)
+        FILE_TYPE_MAP.put("424d8e1b030000000000", "bmp"); //256色位图(bmp)
+        FILE_TYPE_MAP.put("41433130313500000000", "dwg"); //CAD (dwg)
+        FILE_TYPE_MAP.put("3c21444f435459504520", "html"); //HTML (html)
+        FILE_TYPE_MAP.put("3c21646f637479706520", "htm"); //HTM (htm)
+        FILE_TYPE_MAP.put("48544d4c207b0d0a0942", "css"); //css
+        FILE_TYPE_MAP.put("696b2e71623d696b2e71", "js"); //js
+        FILE_TYPE_MAP.put("7b5c727466315c616e73", "rtf"); //Rich Text Format (rtf)
+        FILE_TYPE_MAP.put("38425053000100000000", "psd"); //Photoshop (psd)
+        FILE_TYPE_MAP.put("46726f6d3a203d3f6762", "eml"); //Email [Outlook Express 6] (eml)
+        FILE_TYPE_MAP.put("d0cf11e0a1b11ae10000", "office"); //MS Office文件(doc,excel等)
+        FILE_TYPE_MAP.put("5374616E64617264204A", "mdb"); //MS Access (mdb)
+        FILE_TYPE_MAP.put("252150532D41646F6265", "ps");
+        FILE_TYPE_MAP.put("255044462d312e350d0a", "pdf"); //Adobe Acrobat (pdf)
+        FILE_TYPE_MAP.put("2e524d46000000120001", "rmvb"); //rmvb/rm相同
+        FILE_TYPE_MAP.put("464c5601050000000900", "flv"); //flv与f4v相同
+        FILE_TYPE_MAP.put("00000020667479706d70", "mp4");
+        FILE_TYPE_MAP.put("49443303000000002176", "mp3");
+        FILE_TYPE_MAP.put("000001ba210001000180", "mpg"); //
+        FILE_TYPE_MAP.put("3026b2758e66cf11a6d9", "wmv"); //wmv与asf相同
+        FILE_TYPE_MAP.put("52494646e27807005741", "wav"); //Wave (wav)
+        FILE_TYPE_MAP.put("52494646d07d60074156", "avi");
+        FILE_TYPE_MAP.put("4d546864000000060001", "mid"); //MIDI (mid)
+        FILE_TYPE_MAP.put("504b0304140000000800", "zip");
+        FILE_TYPE_MAP.put("526172211a0700cf9073", "rar");
+        FILE_TYPE_MAP.put("235468697320636f6e66", "ini");
+        FILE_TYPE_MAP.put("504b03040a0000000000", "jar");
+        FILE_TYPE_MAP.put("4d5a9000030000000400", "exe");//可执行文件
+        FILE_TYPE_MAP.put("3c25402070616765206c", "jsp");//jsp文件
+        FILE_TYPE_MAP.put("4d616e69666573742d56", "mf");//MF文件
+        FILE_TYPE_MAP.put("3c3f786d6c2076657273", "xml");//xml文件
+        FILE_TYPE_MAP.put("494e5345525420494e54", "sql");//xml文件
+        FILE_TYPE_MAP.put("7061636b616765207765", "java");//java文件
+        FILE_TYPE_MAP.put("406563686f206f66660d", "bat");//bat文件
+        FILE_TYPE_MAP.put("1f8b0800000000000000", "gz");//gz文件
+        FILE_TYPE_MAP.put("6c6f67346a2e726f6f74", "properties");//bat文件
+        FILE_TYPE_MAP.put("cafebabe0000002e0041", "class");//bat文件
+        FILE_TYPE_MAP.put("49545346030000006000", "chm");//bat文件
+        FILE_TYPE_MAP.put("04000000010000001300", "mxp");//bat文件
+        FILE_TYPE_MAP.put("504b0304140006000800", "docx");//docx文件
+        FILE_TYPE_MAP.put("6431303a637265617465", "torrent");
+        FILE_TYPE_MAP.put("6D6F6F76", "mov"); //Quicktime (mov)
+        FILE_TYPE_MAP.put("FF575043", "wpd"); //WordPerfect (wpd)
+        FILE_TYPE_MAP.put("CFAD12FEC5FD746F", "dbx"); //Outlook Express (dbx)
+        FILE_TYPE_MAP.put("2142444E", "pst"); //Outlook (pst)
+        FILE_TYPE_MAP.put("AC9EBD8F", "qdf"); //Quicken (qdf)
+        FILE_TYPE_MAP.put("E3828596", "pwl"); //Windows Password (pwl)
+        FILE_TYPE_MAP.put("2E7261FD", "ram"); //Real Audio (ram)
     }
 
     /**
@@ -64,10 +124,35 @@ public class CommonUtils {
      * @throws IOException
      */
     public static boolean isBinaryFile(File file) throws IOException {
+        if (file == null || !file.exists() || !file.isFile()) {
+            return false;
+        }
         String type = Files.probeContentType(file.toPath());
         logger.debug("file:{}, type:{}", file.getAbsolutePath(), type);
         if (type == null) {
             return true;
         } else return !type.startsWith("text");
+    }
+
+    /**
+     * 读取二进制文件头
+     *
+     * @param file
+     * @return
+     */
+    public static String readBinaryHead(File file) {
+        if (file == null || !file.exists() || !file.isFile()) {
+            return null;
+        }
+        //读取前10个字节
+        byte[] tmp = new byte[10];
+        try (FileInputStream fis = new FileInputStream(file)) {
+            if (fis.read(tmp, 0, tmp.length) != -1) {
+                return Hex.encodeHexString(tmp);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
